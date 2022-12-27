9442c047b4a36d443309aff647c96b4dc0d6ed8a
edward <bepyan@naver.com>
2022-12-27 19:30:27 +0900

    feat. add base end '/'

--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/quiet-time',
+  base: '/quiet-time/',
   plugins: [react()],
   resolve: {
     alias: {
