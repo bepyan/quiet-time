4677c05d8ed45b261895b78a59c5d83d6bf24633
edward <bepyan@naver.com>
2022-12-27 20:29:07 +0900

    feat. monorepo 변수 확인

--- packages/svelte-kit/src/routes/api/hello/+server.ts
+++ packages/svelte-kit/src/routes/api/hello/+server.ts
@@ -1,15 +1,9 @@
-import { error } from '@sveltejs/kit';
+import { getCommonStr } from '@qt/common';
 
 export function GET() {
-  const product = {
-    name: 'sticker',
-    src: 'https://cdn.shopify.com/s/files/1/0434/0285/4564/products/Cup-front-black.png?v=1623159405',
-    price: '$10',
-  };
-
-  if (!product) {
-    throw error(400, 'No product exists.');
-  }
-
-  return new Response(JSON.stringify(product));
+  return new Response(
+    JSON.stringify({
+      message: getCommonStr(),
+    }),
+  );
 }
